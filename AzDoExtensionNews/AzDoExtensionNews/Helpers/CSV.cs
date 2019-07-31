1f2ee0a11020b2f5506f7caf43851955febf9915
Rob Bos <raj.bos@gmail.com>
2019-07-31 09:42:05 +0200

    Extracted CSV code

---
+++ AzDoExtensionNews/AzDoExtensionNews/Helpers/CSV.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace AzDoExtensionNews.Helpers
+{
+#pragma warning disable S101 // Types should be named in PascalCase
+    internal static class CSV
+#pragma warning restore S101 // Types should be named in PascalCase
+    {
+        public static void SaveCSV(List<Extension> allExtensions)
+        {
+            if (1 == 2) // todo: extract to setting
+            {
+                CreateCSV(allExtensions, "extensions.csv");
+            }
+        }
+
+        private static void CreateHeader<T>(List<T> list, StreamWriter sw)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            for (int i = 0; i < properties.Length - 1; i++)
+            {
+                sw.Write(properties[i].Name + ",");
+            }
+            var lastProp = properties[properties.Length - 1].Name;
+            sw.Write(lastProp + sw.NewLine);
+        }
+
+        private static void CreateRows<T>(List<T> list, StreamWriter sw)
+        {
+            foreach (var item in list)
+            {
+                PropertyInfo[] properties = typeof(T).GetProperties();
+                for (int i = 0; i < properties.Length - 1; i++)
+                {
+                    var prop = properties[i];
+                    sw.Write(prop.GetValue(item) + ",");
+                }
+                var lastProp = properties[properties.Length - 1];
+                sw.Write(lastProp.GetValue(item) + sw.NewLine);
+            }
+        }
+
+        private static void CreateCSV<T>(List<T> list, string filePath)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                CreateHeader(list, sw);
+                CreateRows(list, sw);
+            }
+        }
+    }
+}
--- AzDoExtensionNews/AzDoExtensionNews/Program.cs
+++ AzDoExtensionNews/AzDoExtensionNews/Program.cs
@@ -62,7 +62,7 @@ namespace AzDoExtensionNews
             if ((newExtensions.Any() || updateExtension.Any()) && PostUpdates(newExtensions, updateExtension))
             {
                 // store new data
-                //SaveCSV(extensions);
+                CSV.SaveCSV(extensions);
                 SaveJson(extensions);
             }
         }
@@ -181,51 +181,6 @@ namespace AzDoExtensionNews
 
             return uniqueList;
         }
-
-        #region Save to CSV
-        private static void SaveCSV(List<Extension> allExtensions)
-        {
-            if (1 == 2)
-            {
-                CreateCSV(allExtensions, "extensions.csv");
-            }
-        }
-
-        private static void CreateHeader<T>(List<T> list, StreamWriter sw)
-        {
-            PropertyInfo[] properties = typeof(T).GetProperties();
-            for (int i = 0; i < properties.Length - 1; i++)
-            {
-                sw.Write(properties[i].Name + ",");
-            }
-            var lastProp = properties[properties.Length - 1].Name;
-            sw.Write(lastProp + sw.NewLine);
-        }
-
-        private static void CreateRows<T>(List<T> list, StreamWriter sw)
-        {
-            foreach (var item in list)
-            {
-                PropertyInfo[] properties = typeof(T).GetProperties();
-                for (int i = 0; i < properties.Length - 1; i++)
-                {
-                    var prop = properties[i];
-                    sw.Write(prop.GetValue(item) + ",");
-                }
-                var lastProp = properties[properties.Length - 1];
-                sw.Write(lastProp.GetValue(item) + sw.NewLine);
-            }
-        }
-
-        public static void CreateCSV<T>(List<T> list, string filePath)
-        {
-            using (StreamWriter sw = new StreamWriter(filePath))
-            {
-                CreateHeader(list, sw);
-                CreateRows(list, sw);
-            }
-        }
-        #endregion
 
         private static void LogDataResult(ExtensionDataResult data, int pageNumber, int pageSize)
         {
