955f44736c7693091a2e3f5436a9cf4f3f21fc0b
Rob Bos <raj.bos@gmail.com>
2021-01-18 08:30:45 +0100

    Prevent issues with Twitter Api when tagging people, after account suspencion (without any communication:-| ). Removed @mentioning completely

--- AzDoExtensionNews/AzDoExtensionNews/Helpers/PublisherFinder.cs
+++ AzDoExtensionNews/AzDoExtensionNews/Helpers/PublisherFinder.cs
@@ -3,21 +3,23 @@ using News.Library;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AzDoExtensionNews.Helpers
 {
     public static class PublisherFinder
     {        
-        public static string GetPublisher(Extension extension, List<PublisherHandles> publisherHandles)
+        public static string GetPublisher(Extension extension, List<PublisherHandles> publisherHandles, bool useTwitterHandles = false)
         {
-            var handle = publisherHandles.FirstOrDefault(item => item.PublisherName.Equals(extension.publisher.publisherName, StringComparison.InvariantCultureIgnoreCase));
-            if (handle != null)
+            if (useTwitterHandles)
             {
-                return handle.TwitterHandle;
-            }
+                var handle = publisherHandles.FirstOrDefault(item => item.PublisherName.Equals(extension.publisher.publisherName, StringComparison.InvariantCultureIgnoreCase));
+                if (handle != null)
+                {
+                    return handle.TwitterHandle;
+                }
 
-            Log.Message($"Could not find publisher handle for [{extension.publisher.publisherName}] in the publisher handles list (Count: {publisherHandles.Count})");
+                Log.Message($"Could not find publisher handle for [{extension.publisher.publisherName}] in the publisher handles list (Count: {publisherHandles.Count})");
+            }
             return extension.publisher.displayName;
         }
     }
